ac36bfa7b13d03422c98e7dacf8434ce503d864f
p3podev <jr_garciaparedes@hotmail.com>
2024-07-05 13:11:06 -0500

    Fix cors errors

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// Verificar si ALLOWED_ORIGINS está definido
+if (!process.env.ALLOWED_ORIGINS) {
+  console.error('Error: ALLOWED_ORIGINS no está definido en el archivo .env');
+  process.exit(1); // Salir del proceso con un código de error
+}
+
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim());
-const apiUrl = process.env.API_URL;
+const apiUrls = process.env.API_URL;
 
 // Middleware de seguridad con Helmet
 app.use(
@@ -16,7 +23,7 @@ app.use(
       directives: {
         "default-src": ["'self'"],
         "script-src": ["'self'", "https://cdn.socket.io"],
-        "connect-src": ["'self'", apiUrl, apiUrl.replace('https', 'wss')],
+        "connect-src": ["'self'", ...apiUrls.map(url => url), ...apiUrls.map(url => url.replace('https', 'wss'))],
         "img-src": ["'self'", "data:", "https:"],
       },
     },
@@ -44,7 +51,7 @@ app.get("/", (req, res) => {
 
 app.get("/config", (req, res) => {
   res.json({
-    apiUrl: apiUrl,
+    apiUrls: apiUrls,
   });
 });
 
